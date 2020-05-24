35dd3c411229efa4b6d499be7e6589f4cb5a5d42
DANILO RAMIREZ OSORIO <daniloramirez187435@correo.itm.edu.co>
2020-05-24 12:50:28 -0500

    Order SeedDb

---
+++ Prevent.Web/Data/SeedDb.cs
@@ -0,0 +1,57 @@
+﻿using Prevent.Web.Data.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Prevent.Web.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _dataContext;
+
+        public SeedDb(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _dataContext.Database.EnsureCreatedAsync();
+            PreventTypeEntity Consejo = CheckPreventType("Consejo");
+            PreventTypeEntity Guia = CheckPreventType("Guía de información");
+            await CheckPreventAsync(Consejo, Guia);
+        }
+
+        private async Task CheckPreventAsync(PreventTypeEntity Consejo, PreventTypeEntity Guia)
+        {
+            if (!_dataContext.Prevents.Any())
+            {
+                _dataContext.Prevents.Add(new PreventEntity
+                {
+                    Title = "CONSUMO DE ALCOHOL",
+                    Description = "Cuanta mayor cantidad de alcohol consumas, mayor riesgo de padecer cáncer de mama tienes.",
+                    Date = DateTime.UtcNow,
+                    PreventType = Consejo
+                });
+
+                _dataContext.Prevents.Add(new PreventEntity
+                {
+                    Title = "CONTROLAR EL PESO",
+                    Description = "La gordura y la obesidad aumentan el riesgo de padecer cáncer de mama y más todavía cuando ocurren a mayor edad, particularmente después de la menopausia.",
+                    Date = DateTime.UtcNow,
+                    PreventType = Guia
+                });
+                await _dataContext.SaveChangesAsync();
+            }
+        }
+
+        private PreventTypeEntity CheckPreventType(string Name)
+        {
+            return new PreventTypeEntity
+            {
+                Name = Name
+            };
+        }
+    }
+}
